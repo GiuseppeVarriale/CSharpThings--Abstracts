1cf5e834e76e08ee1624f30237859fa4307849f3
Giuseppe Varriale <pepe.saoleo@gmail.com>
2019-11-20 19:24:32 -0300

    Implement CompanyPayer class

---
+++ Aula _136-AbstractMethods/Entities/CompanyPayer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aula__136_AbstractMethods.Entities
+{
+    class CompanyPayer : TaxPayer
+    {
+        public int EmployeesNumber { get; set; }
+
+        public CompanyPayer()
+        {
+        }
+
+        public CompanyPayer(string name, double anualIncome,int employeesNumber) : base(name, anualIncome)
+        {
+            EmployeesNumber = employeesNumber;
+        }
+
+        public override string TaxesPaid()
+        {
+            int taxPercent = EmployeesNumber > 10 ? 14 : 16;
+            double taxesPaid = taxPercent / 100 * AnualIncome;
+            return $"{Name}: $ {taxesPaid}";
+        }
+    }
+}
