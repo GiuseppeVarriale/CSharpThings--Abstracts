3a38f7a50932d7fd715ad39911fe1c997527bb87
Giuseppe Varriale <pepe.saoleo@gmail.com>
2019-11-20 18:31:50 -0300

    Initial Commit

---
+++ Aula _136-AbstractMethods/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Aula__136_AbstractMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
