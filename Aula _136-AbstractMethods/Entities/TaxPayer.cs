ab4c826f34ec8ca0859a947c988a1ecceae5b798
Giuseppe Varriale <pepe.saoleo@gmail.com>
2019-11-20 18:42:21 -0300

    Create TaxPayer Class

---
+++ Aula _136-AbstractMethods/Entities/TaxPayer.cs
@@ -0,0 +1,11 @@
+﻿namespace Aula__136_AbstractMethods.Entities
+{
+    abstract class TaxPayer
+    {
+        public string Name { get; private set; }
+        public double AnualIncome { get; private set; }
+
+        public abstract string PayTaxes();
+
+    }
+}
