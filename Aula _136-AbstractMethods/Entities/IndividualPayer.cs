64d572f936bc34ed4dd5ca6b9749f98661646459
Giuseppe Varriale <pepe.saoleo@gmail.com>
2019-11-20 18:46:35 -0300

    Create IndiviualPayer class

---
+++ Aula _136-AbstractMethods/Entities/IndividualPayer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aula__136_AbstractMethods.Entities
+{
+    class IndividualPayer : TaxPayer
+    {
+        public double HealthExpenditures { get; private set; }
+
+        public override string PayTaxes()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
